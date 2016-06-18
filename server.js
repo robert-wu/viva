52b25e6ece5c32f8402ec18393384f99b0362c4f
SagarPunhani <sagar7p@gmail.com>
2016-06-18 00:54:02 -0700

    More changes

--- server.js
+++ server.js
@@ -9,11 +9,6 @@ var botConnectorOptions = {
 
 // Create bot
 var bot = new builder.BotConnectorBot(botConnectorOptions);
-bot.add('/', function (session) {
-    
-    //respond with user's message
-    session.send("You said " + session.message.text);
-});
 
 // Setup Restify Server
 var server = restify.createServer();
@@ -30,3 +25,106 @@ server.get(/.*/, restify.serveStatic({
 server.listen(process.env.port || 3978, function () {
     console.log('%s listening to %s', server.name, server.url); 
 });
+
+
+var dialog = new builder.LuisDialog('https://www.luis.ai/application/dbc0fee8-f1bb-4932-a453-98ca65ba1b2c');
+bot.add('/', dialog);
+
+dialog.onBegin(function (session, args, next) {
+    if (!session.userData.firstRun) {
+        // Send the user through the first run experience
+        session.userData.firstRun = true;
+        session.beginDialog('/profile');
+    } else {
+        next();
+    }
+});
+
+dialog.onDefault(builder.DialogAction.send("I'm sorry. I didn't understand."));
+
+dialog.on('GetInformation', [
+    function (session, args) {
+    	var organization = builder.EntityRecognizer.findEntity(args.entities, 'Organization');
+		var medical = builder.EntityRecognizer.findEntity(args.entities, 'Medical');
+    	var criminal = builder.EntityRecognizer.findEntity(args.entities, 'Criminal');
+    	var environmental = builder.EntityRecognizer.findEntity(args.entities, 'Environmental');
+    	if(!organization){
+        	builder.Prompts.text(session, "What would you like me to say?");
+    	}
+    	else if(!medical){
+        	builder.Prompts.text(session, "What would you like me to say?");
+    	}
+    	else if(!criminal){
+        	builder.Prompts.text(session, "What would you like me to say?");
+    	}
+    	else if(!environmental){
+        	builder.Prompts.text(session, "What would you like me to say?");
+    	}
+    	else
+    	{
+
+    	}
+    },
+    function (session, results) {
+        
+
+    }
+]);
+
+dialog.on('ContactOrganization', [
+    function (session, args) {
+    	var organization = builder.EntityRecognizer.findEntity(args.entities, 'Organization');
+		var medical = builder.EntityRecognizer.findEntity(args.entities, 'Medical');
+    	var criminal = builder.EntityRecognizer.findEntity(args.entities, 'Criminal');
+    	var environmental = builder.EntityRecognizer.findEntity(args.entities, 'Environmental');
+    	if(!organization){
+        	builder.Prompts.text(session, "What would you like me to say?");
+    	}
+    	else if(!medical){
+        	builder.Prompts.text(session, "What would you like me to say?");
+    	}
+    	else if(!criminal){
+        	builder.Prompts.text(session, "What would you like me to say?");
+    	}
+    	else if(!environmental){
+        	builder.Prompts.text(session, "What would you like me to say?");
+    	}
+    	else
+    	{
+
+    	}
+    },
+    function (session, results) {
+        
+
+    }
+]);
+{
+  "name": "echobot",
+  "version": "1.0.0",
+  "description": "A sample bot for getting started with Bot Framework",
+  "main": "server.js",
+  "scripts": {
+    "test": ""
+  },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/fuselabs/echobot.git"
+  },
+  "keywords": [
+    "botframework"
+  ],
+  "author": "Fuse Labs",
+  "license": "MIT",
+  "bugs": {
+    "url": "https://github.com/fuselabs/echobot/issues"
+  },
+  "homepage": "https://github.com/fuselabs/echobot#readme",
+  "dependencies": {
+    "botbuilder": "^0.6.5",
+    "restify": "^4.0.4"
+    "botbuilder": "^1.0.1",
+    "request": "^2.72.0",
+    "restify": "^4.1.1"
+  }
+}
