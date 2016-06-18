3a45f0f859596293ecfe4460ada97997be374c70
SagarPunhani <sagar7p@gmail.com>
2016-06-18 00:05:47 -0700

    More testing

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 
-// Create bot and add dialogs
-var bot = new builder.BotConnectorBot({ appId: 'VivaBot', appSecret: '3b6d2413e10843acb75d98da78f8f781' });
-bot.add('/', function (session) {
-    session.send('Hello World');
-});
-
-// Setup Restify Server
 var server = restify.createServer();
-server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
-server.listen(process.env.port || 3978, function () {
+
+var helloBot = new builder.BotConnectorBot();
+helloBot.add('/', new builder.CommandDialog()
+    .matches('^set name', builder.DialogAction.beginDialog('/profile'))
+    .matches('^quit', builder.DialogAction.endDialog())
+    .onDefault([
+        function (session, args, next) {
+            if (!session.userData.name) {
+                session.beginDialog('/profile');
+            } else {
+                next();
+            }
+        },
+        function (session, results) {
+            session.send('Hello %s!', session.userData.name);
+        }
+    ]));
+helloBot.add('/profile',  [
+    function (session) {
+        if (session.userData.name) {
+            builder.Prompts.text(session, 'What would you like to change it to?');
+        } else {
+            builder.Prompts.text(session, 'Hi! What is your name?');
+        }
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        session.endDialog();
+    }
+]);
+
+server.use(helloBot.verifyBotFramework({ appId: 'VivaBot', appSecret: '3b6d2413e10843acb75d98da78f8f781' }));
+server.post('/v1/messages', helloBot.listen());
+
+server.listen(8080, function () {
     console.log('%s listening to %s', server.name, server.url); 
 });
